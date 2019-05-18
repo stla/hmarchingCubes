a762e006ab5a956861bbeb1b7bef46ca83313ec9
stla <laurent_step@yahoo.fr>
2019-05-18 10:56:04 +0200

    normals - pb degenerate faces

--- src/Mesh.hs
+++ src/Mesh.hs
@@ -1,13 +1,17 @@
 module Mesh where
+import           Data.Foldable       (foldl')
+import           Data.IntMap.Strict  (IntMap, elems, unionWith)
+import qualified Data.IntMap.Strict  as IM
 import           Data.List.Split     (chunksOf)
 import           Data.Map.Strict     (insertLookupWithKey)
 import qualified Data.Map.Strict     as M
-import           Data.Matrix         (Matrix, toLists, nrows)
+import           Data.Matrix         (Matrix, nrows, toLists)
 import           Data.Tuple.Extra    (curry3, first, second, thd3, (***))
 import           Data.Vector.Unboxed (Unbox, Vector, fromList, (!))
+import           Linear              (V3 (..), cross, signorm, (^+^), (^-^))
 import           MarchingCubes
 
-type Mesh a = (Vector a, [[Int]])
+type Mesh a = ((Vector (a,a,a), [[Int]]), Vector (a,a,a))
 
 -- matrix2vector :: Unbox a => Matrix a -> Vector (a,a,a)
 -- matrix2vector mtrx = fromList listOfTriples
@@ -26,15 +30,39 @@ uniqueWithIndices list = (fromList *** fromList) $ go 0 M.empty list
         (Nothing, m') -> first (x:) (second (i:) (go (i+1) m' xs))
         (Just j , m') ->             second (j:) (go i     m' xs)
 
-undupMesh :: (Unbox a, Ord a) => ([a], [[Int]]) -> Mesh a
+undupMesh :: (Unbox a, Ord a) => ([(a,a,a)], [[Int]]) -> (Vector (a,a,a), [[Int]])
 undupMesh (vertices, faces) = (vertices', faces')
   where
   (vertices', idx) = uniqueWithIndices vertices
   faces' = map (map (idx !)) faces
 
-makeMesh :: (Unbox a, RealFloat a, Ord a) => Voxel a -> a -> Mesh (a,a,a)
-makeMesh voxel level = undupMesh (vertices, faces)
+normal :: Floating a => (a,a,a) -> (a,a,a) -> (a,a,a) -> V3 a
+normal v1 v2 v3 = signorm $ cross (v3' ^-^ v1') (v2' ^-^ v1')
+  where
+  toV3 (x,y,z) = V3 x y z
+  v1' = toV3 v1
+  v2' = toV3 v2
+  v3' = toV3 v3
+
+faceNormals :: (Floating a, Unbox a) => Vector (a,a,a) -> [Int] -> IntMap (V3 a)
+faceNormals vs face = IM.fromList nrmls
+  where
+  nrml = normal (vs ! (face !! 0)) (vs ! (face !! 1)) (vs ! (face !! 2))
+  nrmls = map (\i -> (i, nrml)) face
+
+normalsV3 :: (Floating a, Unbox a) => (Vector (a,a,a), [[Int]]) -> IntMap (V3 a)
+normalsV3 (vs, faces) =
+  IM.map signorm (foldl' (unionWith (^+^)) IM.empty (map (faceNormals vs) faces))
+
+normals :: (Floating a, Unbox a) => (Vector (a,a,a), [[Int]]) -> Vector (a,a,a)
+normals mesh = fromList $ map fromV3 (elems $ normalsV3 mesh)
+  where
+  fromV3 (V3 x y z) = (x, y, z)
+
+makeMesh :: (Unbox a, RealFloat a, Ord a) => Voxel a -> a -> Mesh a
+makeMesh voxel level = (mesh, normals mesh)
   where
   mtrx = marchingCubes voxel level
   vertices = matrix2listOfTriples mtrx
   faces = chunksOf 3 [0 .. nrows mtrx - 1]
+  mesh = undupMesh (vertices,faces)
